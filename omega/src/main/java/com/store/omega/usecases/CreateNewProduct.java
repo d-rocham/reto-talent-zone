5630b0083b2deb48b6e964f7294339e979cc47a5
d-rocham <d_rocham@outlook.com>
2023-01-19 17:20:27 -0500

    [Feat] Implements CreateNewProduct usecase

---
+++ omega/src/main/java/com/store/omega/usecases/CreateNewProduct.java
@@ -0,0 +1,19 @@
+package com.store.omega.usecases;
+
+import com.store.omega.domain.models.Product;
+import com.store.omega.persistence.InventoryRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CreateNewProduct {
+    private final InventoryRepository inventoryRepository;
+
+    public CreateNewProduct(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public Mono<Product> createNewProduct(Product newProduct) {
+        return inventoryRepository.createProduct(newProduct);
+    }
+}
