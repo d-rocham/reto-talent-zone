f5f2cac8b4f14af06bd75417cc7f6470335a86e3
d-rocham <d_rocham@outlook.com>
2023-01-19 17:20:44 -0500

    [Feat] Implements DeleteProduct usecase

---
+++ omega/src/main/java/com/store/omega/usecases/DeleteProduct.java
@@ -0,0 +1,17 @@
+package com.store.omega.usecases;
+
+import com.store.omega.persistence.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteProduct {
+    private final InventoryRepository inventoryRepository;
+
+    public DeleteProduct(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public void deleteProduct(String targetId) {
+        inventoryRepository.deleteProduct(targetId);
+    }
+}
