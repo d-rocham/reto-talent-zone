6f44dd65cb1f4c9c816b9d004b6179fecc44577f
d-rocham <d_rocham@outlook.com>
2023-01-19 17:29:32 -0500

    [Fix] Adds @Service annotation to modified usecases

--- omega/src/main/java/com/store/omega/usecases/FindAllPurchases.java
+++ omega/src/main/java/com/store/omega/usecases/FindAllPurchases.java
@@ -2,8 +2,10 @@ package com.store.omega.usecases;
 
 import com.store.omega.domain.models.Purchase;
 import com.store.omega.persistence.PurchasesRepository;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+@Service
 public class FindAllPurchases {
     private final PurchasesRepository purchasesRepository;
 
--- omega/src/main/java/com/store/omega/usecases/FindPurchaseById.java
+++ omega/src/main/java/com/store/omega/usecases/FindPurchaseById.java
@@ -2,9 +2,10 @@ package com.store.omega.usecases;
 
 import com.store.omega.domain.models.Purchase;
 import com.store.omega.persistence.PurchasesRepository;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-
+@Service
 public class FindPurchaseById {
     private final PurchasesRepository purchasesRepository;
 
