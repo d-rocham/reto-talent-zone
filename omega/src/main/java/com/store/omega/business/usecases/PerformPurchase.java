73a234630517e0876c5b1ee8abd6648abb17549c
d-rocham <d_rocham@outlook.com>
2023-01-20 18:54:47 -0500

    [Fix] Debugs usecas·

--- omega/src/main/java/com/store/omega/business/usecases/PerformPurchase.java
+++ omega/src/main/java/com/store/omega/business/usecases/PerformPurchase.java
@@ -6,11 +6,13 @@ import com.store.omega.business.dto.PurchaseDTO;
 import com.store.omega.domain.models.Purchase;
 import com.store.omega.persistence.InventoryRepository;
 import com.store.omega.persistence.PurchasesRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 
+@Slf4j
 @Service
 public class PerformPurchase {
     private final InventoryRepository inventoryRepository;
@@ -24,22 +26,28 @@ public class PerformPurchase {
     public Mono<PurchaseDTO> performPurchase(Mono<PurchaseDTO> newPurchase){
         return newPurchase.map(PurchaseBO::new)
                 .doOnNext(purchaseBO -> purchaseBO.getPurchasedProducts().forEach(
-                        purchasedProduct -> this.inventoryRepository
-                                .getProductById(purchasedProduct.getId())
-                                .map(ProductBO::new)
-                                .doOnNext(productBO -> {
-                                    try {
-                                        productBO.setInInventory(purchasedProduct.getAmount());
-                                    } catch (Exception e) {
-                                        throw new RuntimeException(e);
-                                    }
-                                    if (!productBO.isEnabled() || purchasedProduct.getAmount() > productBO.getMax() || purchasedProduct.getAmount() < productBO.getMin()) {
-                                        throw new RuntimeException("Error: Product not available");
-                                    }})
-                                .onErrorResume(throwable -> Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString())))
-                                .map(productBO -> this.inventoryRepository
-                                        .modifyProductInventory(productBO.getId(), productBO.getInInventory()))
-                )).flatMap(purchaseBO -> this.purchasesRepository.createPurchase(new Purchase(purchaseBO)))
+                        purchasedProduct -> {
+                            this.inventoryRepository
+                                    .getProductById(purchasedProduct.getId())
+                                    .map(ProductBO::new)
+                                    .subscribe(productBO -> {
+                                        try {
+                                            productBO.setInInventory(purchasedProduct.getAmount());
+                                        } catch (Exception e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        if (!productBO.isEnabled() || purchasedProduct.getAmount() > productBO.getMax() || purchasedProduct.getAmount() < productBO.getMin()) {
+                                            throw new RuntimeException("Error: Product not available");
+                                        }
+                                        this.inventoryRepository.modifyProductInventory(
+                                                        productBO.getId(),
+                                                        productBO.getInInventory())
+                                                .subscribe();
+                                    });
+                        }
+                ))
+                .onErrorResume(throwable -> Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString())))
+                .flatMap(purchaseBO -> this.purchasesRepository.createPurchase(new Purchase(purchaseBO)))
                 .map(PurchaseDTO::new);
     }
 
