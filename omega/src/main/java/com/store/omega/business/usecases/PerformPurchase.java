7dfc7d4dc693bc5a1875745272f6ab095787400b
d-rocham <d_rocham@outlook.com>
2023-01-20 15:44:30 -0500

    [Refactor] Adapts PerformPurchase usecase to new BO, DTO

--- omega/src/main/java/com/store/omega/business/usecases/PerformPurchase.java
+++ omega/src/main/java/com/store/omega/business/usecases/PerformPurchase.java
@@ -1,6 +1,8 @@
 package com.store.omega.business.usecases;
 
 import com.store.omega.business.businessobjects.ProductBO;
+import com.store.omega.business.businessobjects.PurchaseBO;
+import com.store.omega.business.dto.PurchaseDTO;
 import com.store.omega.domain.models.Purchase;
 import com.store.omega.persistence.InventoryRepository;
 import com.store.omega.persistence.PurchasesRepository;
@@ -19,29 +21,26 @@ public class PerformPurchase {
         this.purchasesRepository = purchasesRepository;
     }
 
-    public Mono<Purchase> performPurchase(Purchase newPurchase){
-
-        newPurchase.getPurchasedProducts().forEach(
-                purchasedProduct -> this.inventoryRepository
-                        .getProductById(purchasedProduct.getId())
-                        .map(ProductBO::new)
-                        .doOnNext(productBO -> {
-                            try {
-                                productBO.setInInventory(purchasedProduct.getAmount());
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                            if (!productBO.isEnabled() || purchasedProduct.getAmount() > productBO.getMax() || purchasedProduct.getAmount() < productBO.getMin()) {
-                                throw new RuntimeException("Error: Product not available for sale");
-                            }
-                        })
-                        .onErrorResume(throwable -> Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString())))
-                        .map(productBO -> this.inventoryRepository
-                                .modifyProductInventory(productBO.getId(), productBO.getInInventory()))
-
-        );
-
-        return purchasesRepository.createPurchase(newPurchase);
+    public Mono<PurchaseDTO> performPurchase(Mono<PurchaseDTO> newPurchase){
+        return newPurchase.map(PurchaseBO::new)
+                .doOnNext(purchaseBO -> purchaseBO.getPurchasedProducts().forEach(
+                        purchasedProduct -> this.inventoryRepository
+                                .getProductById(purchasedProduct.getId())
+                                .map(ProductBO::new)
+                                .doOnNext(productBO -> {
+                                    try {
+                                        productBO.setInInventory(purchasedProduct.getAmount());
+                                    } catch (Exception e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                    if (!productBO.isEnabled() || purchasedProduct.getAmount() > productBO.getMax() || purchasedProduct.getAmount() < productBO.getMin()) {
+                                        throw new RuntimeException("Error: Product not available");
+                                    }})
+                                .onErrorResume(throwable -> Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString())))
+                                .map(productBO -> this.inventoryRepository
+                                        .modifyProductInventory(productBO.getId(), productBO.getInInventory()))
+                )).flatMap(purchaseBO -> this.purchasesRepository.createPurchase(new Purchase(purchaseBO)))
+                .map(PurchaseDTO::new);
     }
 
 }
