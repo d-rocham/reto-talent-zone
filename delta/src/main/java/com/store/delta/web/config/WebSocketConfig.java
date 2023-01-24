8dc6aa7230d5ccfdeadb05c399b08049392e8fde
d-rocham <d_rocham@outlook.com>
2023-01-24 12:02:21 -0500

    [Feat] Configures web socket

---
+++ delta/src/main/java/com/store/delta/web/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.store.delta.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfig {
+    public WebSocketConfig() {
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
